281436795eb3f649983528570f46d7c629462b5e
Johan Euphrosine <proppy@google.com>
2016-11-29 11:27:23 -0800

    brillo-samples/basic: import simpleui
    - add use-library
    - update board image
    - update README
    - update androidthings import
    - remove java8 dep
    
    Change-Id: I70c07a5bb9e41a1bbf0ccb4c1535c859cc8cda79

---
+++ README.md
@@ -0,0 +1,52 @@
+Android Things Simple UI
+========================
+
+This utility shows on the attached display Android [Switch](https://developer.android.com/reference/android/widget/Switch.html) widgets
+for each GPIO pins exported by the board.
+
+When you click or tap on the switch, the GPIO active status switch from HIGH to LOW according to the widget state.
+
+You can attach LEDs or other digital output peripheral to your GPIO pins and toggle them on and off without changing a single line of code
+(if you connect LEDs: make sure you add 470 ohms current-limiting resistors in series to avoid damaging them).
+
+Pre-requisites
+--------------
+
+- Android Things compatible board
+- Display with touch input or external pointing device
+- Android Studio 2.2+
+- LEDs
+- resistors
+- Breadboard
+
+Build and install
+=================
+
+On Android Studio, click on the "Run" button.
+
+If you prefer to run on the command line, type
+
+```bash
+./gradlew installDebug
+adb shell am start com.example.androidthings.simpleui/.SimpleUiActivity
+```
+
+License
+-------
+
+Copyright 2016 The Android Open Source Project, Inc.
+
+Licensed to the Apache Software Foundation (ASF) under one or more contributor
+license agreements.  See the NOTICE file distributed with this work for
+additional information regarding copyright ownership.  The ASF licenses this
+file to you under the Apache License, Version 2.0 (the "License"); you may not
+use this file except in compliance with the License.  You may obtain a copy of
+the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+License for the specific language governing permissions and limitations under
+the License.
---
+++ app/src/main/java/com/example/androidthings/simpleui/SimpleUiActivity.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2016, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.androidthings.simpleui;
+
+import android.app.Activity;
+import com.google.androidthings.pio.Gpio;
+import com.google.androidthings.pio.PeripheralManagerService;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+
+import java.io.IOException;
+
+public class SimpleUiActivity extends Activity {
+
+    private static final String TAG = SimpleUiActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LinearLayout gpioPinsView = (LinearLayout)findViewById(R.id.gpio_pins);
+        try {
+            PeripheralManagerService pioService = new PeripheralManagerService();
+            for (String s : pioService.getGpioList()) {
+                final Gpio ledPin = pioService.openGpio(s);
+                ledPin.setActiveType(Gpio.ACTIVE_HIGH);
+                Switch button = new Switch(this);
+                button.setText(s);
+                button.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        try {
+                            ledPin.setValue(isChecked);
+                        } catch (IOException e) {
+                            Log.e(TAG, "pio error:", e);
+                        }
+                    }
+                });
+                gpioPinsView.addView(button);
+            }
+            Log.d(TAG, "gpio:" + pioService.getGpioList());
+
+        } catch (IOException e) {
+            Log.e(TAG, "pio error:", e);
+        }
+    }
+}
